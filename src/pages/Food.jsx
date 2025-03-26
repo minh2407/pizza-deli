b34545c88a7815da36bba547f3b4c4aeca9c99c3
MinhLy <104962536+minh2407@users.noreply.github.com>
2025-03-26 23:05:42 +0700

    food file added (#1)
    * food file added
    
    * fix something
    
    ---------
    
    Co-authored-by: ly tran bao minh <minh@lys-Mac-mini.local>

---
+++ src/pages/Food.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Food() {
+    
+    return (
+        <div>
+            HI there
+            <h1>I am from Viet Nam</h1>
+        </div>
+    )
+}
